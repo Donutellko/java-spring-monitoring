ba274e047ed3b2c2ca31f5372f1d92720c143ded
Donat_Shergalis <donat_shergalis@epam.com>
2023-09-22 18:45:01 +0200

    add logging for headers in price-application

--- price-service/src/main/java/bitxon/price/controller/PriceController.java
+++ price-service/src/main/java/bitxon/price/controller/PriceController.java
@@ -1,9 +1,17 @@
 package bitxon.price.controller;
 
 import bitxon.price.api.Price;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/price")
@@ -11,8 +19,16 @@ public class PriceController {
     private static final Logger LOG = LoggerFactory.getLogger(PriceController.class);
 
     @GetMapping("/{identifier}")
-    public Price getPrice(@PathVariable("identifier") String identifier) {
+    public Price getPrice(HttpServletRequest request,
+                          @PathVariable("identifier") String identifier) {
         LOG.info("Handling: GET /price/{}", identifier);
+        var headers = Collections.list(request.getHeaderNames()).stream()
+            .collect(Collectors.toMap(Function.identity(),
+                (header) -> Collections.list(request.getHeaders(header))));
+        headers.forEach((key, value) -> {
+            var headerValues = String.join(", ", value);
+            System.out.println(String.format("%s = %s", key, headerValues));
+        });
 
         var amount = switch (identifier) {
             case "ipad" -> 499;
